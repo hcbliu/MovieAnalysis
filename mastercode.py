bde7e4625d12f5f26488b99c14c471d683b8825a
kaylareiman1 <57274801+kaylareiman1@users.noreply.github.com>
2020-02-15 16:48:01 -0500

    This commit should make two changes: 1) Adding some preliminary code to the master file and 2) Adding a folder of Oscars datasets to our repository

--- mastercode.py
+++ mastercode.py
@@ -1,1 +1,28 @@
 #This .py file is where we will create our final project.
+
+##################
+# Movie Analysis #
+##################
+#Authors: Sinduja Sriskanda, Hui-Chen Betty Liu, Kamaneeya Kalaga, Kayla Reiman
+
+
+####################
+# Import Libraries #
+####################
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+############################
+# Read Oscar Data from CSV #
+############################
+
+###Dataset 1 of 4: Pictures###
+#Create dataframe from csv
+OscarsPictures = pd.read_csv('pictures.csv')
+
+#View column names as a list
+print(OscarsPictures.columns)
+
+#View movie titles for first 5 observations
+print(OscarsPictures['name'].head())
